a9fd1610ec4a09d1f02e8a673afb1bfbc8ca9727
jamesabradley04 <114118804+jamesabradley04@users.noreply.github.com>
2025-04-22 22:32:32 -0400

    Update image_to_phantom.py
    commenting code

--- Helpers/image_to_phantom.py
+++ Helpers/image_to_phantom.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 
 # %%
 # {name : [image_path, whitematter_color, graymatter_color]}
+# this function gives us all of the information about the images so they can be displayed, it also contains the RBG values of the gray and white matter, which we determined by examining 
+# individual pixels in the images
 def get_image_info():
     return {
         "CoronalBrain1": [
@@ -46,7 +48,7 @@ def get_image_info():
 
 # %%
 ## Convert image to phantom
-def img_to_phantom(
+def img_to_phantom( # inputs are al of the variable parameters from the GUI, and the image path
     img_path,
     name,
     whitematter_color,
@@ -67,23 +69,16 @@ def img_to_phantom(
     dbg=False,
     fileloc="../BrainPhantoms/",
     skull_thickness=15,
-):
-    ## -------------------------------------------------
+)
+
     ## Check if the phantom already exists
-    ## -------------------------------------------------
-
     query = f"{name}__{tolerance_pct}_{brain_bound_padding}_{background_val}_{whitematter_val}_{graymatter_val}_{csf_val}_{p_whitematter_val}_{p_graymatter_val}_{p_csf_val}_b_{blur_radius}_t_{tumor_params}_s_{skull_thickness}_{c_skull_val}_{p_skull_val}"
-
     if os.path.exists(f"{fileloc}{query}.npy"):
         if not dbg:
             return np.load(f"{fileloc}{query}.npy"), query
 
-    ## -------------------------------------------------
-    ## Load the image & remove the number from the top left corner
-    ## -------------------------------------------------
-
     img = Image.open(img_path).convert("RGBA")
-    r, g, b, _ = img.split()
+    r, g, b, _ = img.split() # isolates channels
 
     # Remove the number from top left corner of the image
     for x in range(img.width):
@@ -96,7 +91,7 @@ def img_to_phantom(
                 img.putpixel((x, y), (0, 0, 0, 0))
 
 ## -------------------------------------------------
-## Pad the y directions
+## Pad the y directions (had to do this to eliminate artifacts in the iradonttrasnform, as parts of the phantom were outside the FOV
 ## -------------------------------------------------
     rows_to_add = img.width - img.height
     
@@ -107,17 +102,16 @@ def img_to_phantom(
 
     r, g, b, a = img.split()
 
-    ## -------------------------------------------------
-    ## Find whitematter and gray matter pixels
-    ## -------------------------------------------------
 
-    whitematter_img = Image.new("L", img.size, 0)
+    ## Find whitematter and gray matter pixels
+
+    whitematter_img = Image.new("L", img.size, 0) # initilize empty images to serve as white matter and gray matter
     graymatter_img = Image.new("L", img.size, 0)
 
-    for x in range(img.width):
+    for x in range(img.width):  # note, tolerance percent is set to 8% in function inputs
         for y in range(img.height):
-            is_whitematter_r = (
-                whitematter_color[0] - tolerance_pct * whitematter_color[0] / 100
+            is_whitematter_r = ( # checks to make sure the pixel is in an acceptable range for white matter's red channel value, the rest work similiarly
+                whitematter_color[0] - tolerance_pct * whitematter_color[0] / 100 
                 <= r.getpixel((x, y))
                 <= whitematter_color[0] + tolerance_pct * whitematter_color[0] / 100
             )
@@ -148,7 +142,7 @@ def img_to_phantom(
                 <= graymatter_color[2] + tolerance_pct * graymatter_color[2] / 100
             )
 
-            if is_whitematter_r and is_whitematter_g and is_whitematter_b:
+            if is_whitematter_r and is_whitematter_g and is_whitematter_b: # works out if each pixel is white or gray, and adds it to the corresponding blank image
                 whitematter_img.putpixel((x, y), 1)
             elif is_graymatter_r and is_graymatter_g and is_graymatter_b:
                 graymatter_img.putpixel((x, y), 1)
@@ -157,12 +151,12 @@ def img_to_phantom(
     graymatter_array = np.array(graymatter_img)
 
     # make copies for PET phantoms
-    p_whitematter_array = whitematter_array.copy() * p_whitematter_val
-    p_graymatter_array = graymatter_array.copy() * p_graymatter_val
+    p_whitematter_array = whitematter_array.copy() * p_whitematter_val # multiplies by whitematter metabolic rate
+    p_graymatter_array = graymatter_array.copy() * p_graymatter_val # graymatter metabolic rate
 
     # assign values to CT phantoms
-    whitematter_array = whitematter_array * whitematter_val
-    graymatter_array = graymatter_array * graymatter_val
+    whitematter_array = whitematter_array * whitematter_val # white matter density
+    graymatter_array = graymatter_array * graymatter_val # gray matter density
 
     wg_array = np.add(whitematter_array, graymatter_array)  # Combine the two arrays
     
@@ -171,7 +165,7 @@ def img_to_phantom(
     ## -------------------------------------------------
 
     rows, cols = np.where(wg_array != 0)  # find the size of the brain
-    min_x = np.min(cols) - brain_bound_padding
+    min_x = np.min(cols) - brain_bound_padding # brain bound padding set to 10 in inputs
     max_y = np.max(rows) + brain_bound_padding
     max_x = np.max(cols) + brain_bound_padding
     min_y = np.min(rows) - brain_bound_padding
@@ -186,11 +180,12 @@ def img_to_phantom(
     csf_array = background_array.copy()  # Get the oval
     csf_oval = csf_array.copy()  # Make a copy for CSF
     csf_array[wg_array != 0] = 0  # Ensure no overlap with brain
+    
 
     p_csf_array = csf_array.copy()  # Make a copy for PET phantoms
     csf_array = csf_array * csf_val  # Assign CSF value
 
-    if dbg:
+    if dbg: # development feature for debugging and modification
         for image, title in zip(
             [csf_array, whitematter_array, graymatter_array, background_array],
             ["CSF Layer", "Whitematter Layer", "Graymatter Layer", "BKG Layer"],
@@ -212,9 +207,7 @@ def img_to_phantom(
     )
     skull_mask[csf_oval != 0] = 0  # Ensure no overlap with CSF
 
-    ## -------------------------------------------------
-    ## add blur to each layer
-    ## -------------------------------------------------
+    # adding gaussian blur to all layers in oder to decrease simulated resolution
     # CT
     skull_mask_c = skull_mask.copy() * c_skull_val
     skull_mask_p = skull_mask.copy() * p_skull_val
@@ -231,13 +224,11 @@ def img_to_phantom(
     skull_mask = Image.fromarray(skull_mask_c.astype(np.uint8)).filter(
         ImageFilter.GaussianBlur(radius=blur_radius)
     )
-    
-
     whitematter_array = np.array(whitematter_array)
     graymatter_array = np.array(graymatter_array)
     csf_array = np.array(csf_array)
     skull_mask = np.array(skull_mask)
-
+    
     # PET
     p_whitematter_array = Image.fromarray(p_whitematter_array.astype(np.uint8)).filter(
         ImageFilter.GaussianBlur(radius=blur_radius)
@@ -256,19 +247,16 @@ def img_to_phantom(
     p_graymatter_array = np.array(p_graymatter_array)
     p_csf_array = np.array(p_csf_array)
 
-    ## -------------------------------------------------
     ## Add tumor if specified
-    ## -------------------------------------------------
-
     if tumor_params is not None:
         x, y, w, h, val, p_val = tumor_params
-        tumor_array = np.zeros_like(whitematter_array)
+        tumor_array = np.zeros_like(whitematter_array) #overalay for ct (needs its density value, not metabolic, thats why we need to do these things separeaely)
         create_oval(tumor_array, x, y, w, h, val)
 
-        p_tumor_array = np.zeros_like(p_whitematter_array)
+        p_tumor_array = np.zeros_like(p_whitematter_array) # tumor overalay for pet
         create_oval(p_tumor_array, x, y, w, h, p_val)
 
-        tumor_array = Image.fromarray(tumor_array).filter(
+        tumor_array = Image.fromarray(tumor_array).filter( # add blur same as before
             ImageFilter.GaussianBlur(radius=blur_radius)
         )
         tumor_array = np.array(tumor_array)
@@ -278,11 +266,9 @@ def img_to_phantom(
         )
         p_tumor_array = np.array(p_tumor_array)
 
-    ## -------------------------------------------------
-    ## Combine the layers
-    ## -------------------------------------------------
 
-    result_ct = whitematter_array + graymatter_array + csf_array + skull_mask
+    ## Combine the layers
+    result_ct = whitematter_array + graymatter_array + csf_array + skull_mask # adds all arrays together to create the phantom withh all elements
     if tumor_params is not None:
         result_ct += tumor_array
     result_ct = np.array(result_ct)
@@ -292,7 +278,7 @@ def img_to_phantom(
         result_pet += p_tumor_array
     result_pet = np.array(result_pet)
 
-    result = [result_ct, result_pet]
+    result = [result_ct, result_pet] # returns the two phantoms which are assigned to variabbles and used in the main code
 
     if not dbg:
         np.save(f"{fileloc}{query}.npy", result)  # Save the phantom
@@ -303,7 +289,7 @@ def img_to_phantom(
 # %%
 def create_oval(arr, x, y, w, h, val):
     rows, cols = arr.shape
-    y_g, x_g = np.ogrid[-y : rows - y, -x : cols - x]
+    y_g, x_g = np.ogrid[-y : rows - y, -x : cols - x] # similar function to meshgrid
     mask = (x_g**2 / (w / 2) ** 2) + (y_g**2 / (h / 2) ** 2) <= 1
     arr[mask] = val
 
@@ -312,13 +298,36 @@ def pet_sim(image, decay, fluence, exposure_time):
     # Generate Poisson-distributed emissions
     decays = np.random.poisson(image * exposure_time * decay)
     
-    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
+    theta = np.linspace(0., 180., max(image.shape), endpoint=False) # all the code and workflow from 3B
     sinogram = radon(decays, theta=theta)
     
     output = iradon(sinogram, filter_name="hamming", circle=True)
     
     return sinogram, output
 
+
+
+""" included the original attempt at pet_sim, which simulated individual emissions as lines across the image (ie a point in l, theta)
+def pet_sim(image, emissions, blank):
+    blank_sinogram = radon(blank) # make empty sinogram
+    for i in range(len(image)):
+        for j in range(len(image[0])):
+            lines = int(np.round(image[i, j] / image.max() * emissions)) # iterate throuugh the array work out how many emissions a pixel should have
+            for k in range(lines):
+                angle = np.random.randint(0, 180) 
+                theta = np.deg2rad(angle)
+                height, width = image.shape
+                y = i - height / 2
+                x = j - width / 2
+                l = int(np.round(x * np.cos(theta) + y * np.sin(theta))) # convert xy values of pixel to L value for given theta
+                l_index = l + blank_sinogram.shape[0] // 2
+                if 0 <= l_index < blank_sinogram.shape[0]: # add one to the sinogram, indicating that at that l and theta value, a line was detected
+                    blank_sinogram[l_index, angle] += 1
+    return blank_sinogram 
+    # would have then done iradon on this. results were good, however computation time was ridiculous on both our laptops when integrated with main code
+    # the above updated version is very efficient, and provides the same results, just not as directly
+
+# some code used for testing original pet_sim
 # img_data = pet_sim(np.ones((100, 100)), 1, np.zeros((100, 100)))
 # img_data = np.clip(img_data, 0, 255).astype(np.uint8)
 # img = Image.fromarray(img_data)
@@ -329,3 +338,4 @@ def pet_sim(image, decay, fluence, exposure_time):
 # img2 = np.clip(img2, 0, 255).astype(np.uint8)
 # img2 = Image.fromarray(img2)
 # img2.save("phan.png")
+"""
